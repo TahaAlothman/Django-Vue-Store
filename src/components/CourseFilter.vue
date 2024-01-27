a336746f74a184aec5cd00dda32b236a660905e6
[TahaAlothman] <tahaaborawi@gmail.com>
2024-01-27 15:22:20 +0100

    add Filter courses Component

--- src/components/CourseFilter.vue
+++ src/components/CourseFilter.vue
@@ -1,9 +1,40 @@
 <template>
-  <h3>Course Filter</h3>
+  <div class="container">
+    <h4>Course Filter</h4>
+    <form>
+      <div class="mt-3">
+        <label for=""  class="mb-3">Filter by Category</label>
+      <div v-for="cat in category" :key="cat.id">
+        <input type="checkbox"
+        :value="cat.id"
+        class="form-check-input checkbox-item"
+        :id="cat.id"
+        >
+        <label :for="cat.id" class="form.label">{{ cat.name }}</label>
+      </div>
+      </div>
+    </form>
+  </div>
+  
 </template>
 
 <script>
   export default {
-    name: 'CourseFilter'
+    name: 'CourseFilter',
+    data(){
+      return{
+        category:[
+          {id:1, name:'Web Development'},
+          {id:2, name:'Mobile Development'},
+          {id:3, name:'AI Development'},
+        ]
+      }
+    }
+
   }
 </script>
+<style>
+  .checkbox-item{
+    margin-right: 10px;
+  }
+</style>
